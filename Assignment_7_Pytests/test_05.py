5122872236cf70e45748597da721b975f0535a85
amankumar62030 <amankumarakk536@gmail.com>
2025-02-02 12:45:33 +0530

    2/2/25

---
+++ Assignment_7_Pytests/test_05.py
@@ -0,0 +1,7 @@
+# Task5: Form Submission and Error Message
+# Write a PyTest test that:
+# 1.	Opens a form page (e.g., "https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit").
+# 2.	Submits the form with invalid data.
+# 3.	Verifies if an error message appears.
+
+
--- Framework/testCases/test_login.py
+++ Framework/testCases/test_login.py
@@ -1,30 +1,37 @@
-<<<<<<< HEAD
-import time
 
+import time
 import pytest
 from selenium import webdriver
-
-from PageObjects.LoginPage import LoginPage
+from Framework.PageObjects.LoginPage import LoginPage
+from Framework.utilities.readProperties import ReadConfig
+from Framework.utilities.customLogger import LogGen
 
 class Test_001_Login:
-    baseURL = "https://admin-demo.nopcommerce.com/admin/"
-    username = "admin@yourstore.com"
-    password = "admin"
+    baseURL =  ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+
+    logger = LogGen.loggen()
 
 
     def test_homePageTitle(self,setup):
+        self.logger.info("**********************Test_001_Login***************************")
+        self.logger.info("**********************Verifying home page title***************************")
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
         if act_title=="Your store. Login":
             assert True
             self.driver.close()
+            self.logger.info("*****************Home page title test is passed********************")
         else:
             self.driver.save_screenshot(r"D:\BEBO_TECHNOLOGY\Framework\Screenshots\test_homePageTitle.png")
             self.driver.close()
+            self.logger.error("***************Home page title test is failed***************")
             assert False
 
     def test_Login(self,setup):
+        self.logger.info("***************Verifying Login test***************")
         self.driver = setup
         self.driver.get(self.baseURL)
         self.lp = LoginPage(self.driver)
@@ -35,57 +42,12 @@ class Test_001_Login:
         act_title = self.driver.title
         if act_title=="Dashboard / nopCommerce administration":
             assert True
+            self.logger.info("***************Login test is passed***************")
             self.driver.close()
         else:
             self.driver.save_screenshot(r"D:\BEBO_TECHNOLOGY\Framework\Screenshots\test_Login.png")
             self.driver.close()
+            self.logger.error("***************Login test is Failed***************")
             assert False
 
 
-
-=======
-import time
-
-import pytest
-from selenium import webdriver
-
-from PageObjects.LoginPage import LoginPage
-
-class Test_001_Login:
-    baseURL = "https://admin-demo.nopcommerce.com/admin/"
-    username = "admin@yourstore.com"
-    password = "admin"
-
-
-    def test_homePageTitle(self,setup):
-        self.driver = setup
-        self.driver.get(self.baseURL)
-        act_title = self.driver.title
-        if act_title=="Your store. Login":
-            assert True
-            self.driver.close()
-        else:
-            self.driver.save_screenshot(r"D:\BEBO_TECHNOLOGY\Framework\Screenshots\test_homePageTitle.png")
-            self.driver.close()
-            assert False
-
-    def test_Login(self,setup):
-        self.driver = setup
-        self.driver.get(self.baseURL)
-        self.lp = LoginPage(self.driver)
-        self.lp.setUserName(self.username)
-        self.lp.setUserPassword(self.password)
-        self.lp.clickLogin()
-        time.sleep(5)
-        act_title = self.driver.title
-        if act_title=="Dashboard / nopCommerce administration":
-            assert True
-            self.driver.close()
-        else:
-            self.driver.save_screenshot(r"D:\BEBO_TECHNOLOGY\Framework\Screenshots\test_Login.png")
-            self.driver.close()
-            assert False
-
-
-
->>>>>>> b80ed77 (30/1/25)
