43fdf12da57eeab13f91355b01318ba8d7d84f94
amankumar62030 <amankumarakk536@gmail.com>
2025-02-04 11:03:20 +0530

    4/2/25

--- python/Searching_Algorithm/binary_search.py
+++ python/Searching_Algorithm/binary_search.py
@@ -1,21 +1,30 @@
-my_lst = list(map(int, input("Enter the elements of list separated by commas: ").split(",")))
+def binary_search(my_list,target):
+    my_list.sort()
+    low = 0
+    high = len(my_list)-1
+
+    while low<=high:
+        mid = (low+high)//2
+
+        if my_list[mid] == target:
+            print(f"Element {target} is present at index {mid}")
+            break
+        elif my_list[mid] < target:
+            low =mid+1
+        else:
+            high =mid-1
+
+    if low>high:
+        print("Element is not present")
+
+my_list = list(map(int,input("Enter the element separated by comma: ").split(",")))
 target = int(input("Enter the target element: "))
+binary_search(my_list,target)
 
-my_lst.sort()
 
-low = 0
-high = len(my_lst) - 1
 
-while low <= high:
-    mid = low + high//2
-
-    if my_lst[mid] == target:
-        print(f"Element {target} is present at index {mid}")
-        break
-    elif my_lst[mid] < target:
-        low = mid + 1
-    else:
-        high = mid - 1
-
-if low > high:
-    print(f"Element {target} is not present in the list.")
+# Binary Search (Only for Sorted Arrays)
+# Best Case: O(1) (Middle element is the target)
+# Worst Case: O(log n) (Halving the array at each step)
+# Average Case: O(log n)
+# Space Complexity: O(1) (Iterative) / O(log n) (Recursive due to function call stack)
--- python/Sorting_algorithm/bubble_sort.py
+++ python/Sorting_algorithm/bubble_sort.py
@@ -1,7 +1,7 @@
 my_list = list(map(int, input("Enter the elements separated by commas: ").split(",")))
 print("Unsorted list: ",my_list)
 
-for i in range(len(my_list)-1):
+for i in range(len(my_list)):
     for j in range(0, len(my_list) - 1):
         if my_list[j] > my_list[j + 1]:
             my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
