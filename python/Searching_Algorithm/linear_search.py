4b5b972cab32b14fd3cd4cf0c8a591485ea14453
amankumar62030 <amankumarakk536@gmail.com>
2025-02-04 11:03:33 +0530

    4/2/25

--- python/Searching_Algorithm/linear_search.py
+++ python/Searching_Algorithm/linear_search.py
@@ -1,10 +1,18 @@
-my_lst = list(map(int,input("Enter the elements of list separated by commas: ").split(",")))
-target = int(input("Enter the target element: "))
-
-for i in range(len(my_lst)):
-    if target in my_lst:
-        print(f"The element {target} is present in the list")
-        break
+def find_element(my_list,target):
+    for i in range(len(my_list)):
+        if my_list[i]==target:
+            print(f"Element {target} is present at index {i}")
+            break
     else:
-        print(f"The element {target} is not present in the list")
-        break
+        print(f"Element {target} is not present in the list")
+
+my_list = list(map(int,input("Enter the element separated by commas: ").split(",")))
+target = int(input("Enter the target element :"))
+find_element(my_list,target)
+
+
+# Linear Search
+# Best Case: O(1) (Element found at the beginning)
+# Worst Case: O(n) (Element found at the end or not present)
+# Average Case: O(n)
+# Space Complexity: O(1) (No extra space required)
