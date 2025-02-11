a4e10f30ca08453ed851e2ea693440299a6c16a1
amankumar62030 <amankumarakk536@gmail.com>
2025-02-11 15:40:20 +0530

    9/2/25

--- python/Sorting_algorithm/bubble_sort.py
+++ python/Sorting_algorithm/bubble_sort.py
@@ -1,9 +1,9 @@
-my_list = list(map(int, input("Enter the elements separated by commas: ").split(",")))
-print("Unsorted list: ",my_list)
+my_list = list(map(int,input("Enter the element separated by commas: ").split(",")))
+print("Unordered list: ",my_list)
 
 for i in range(len(my_list)):
-    for j in range(0, len(my_list) - 1):
-        if my_list[j] > my_list[j + 1]:
-            my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+    for j in range(len(my_list)-1):
+        if my_list[j]>my_list[j+1]:
+            my_list[j],my_list[j+1]=my_list[j+1],my_list[j]
 
-print("Sorted List:", my_list)
+print("Sorted using bubble sort: ",my_list)
