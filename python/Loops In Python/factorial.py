f5eeef5251bb6a23cac6f162da433d3436d7e0e6
amankumar62030 <amankumarakk536@gmail.com>
2025-02-12 20:51:59 +0530

    9/2/25

--- python/Loops In Python/factorial.py
+++ python/Loops In Python/factorial.py
@@ -1,9 +1,10 @@
+def factorial(n):
+    if n == 0 and n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
-n = int(input("Enter the number:"))
-
-fact = 1
-
-for i in range(1, n+1):
-    fact*=i
-print(fact)
+n = int(input("Enter the number: "))
+result = factorial(n)
+print(result)
